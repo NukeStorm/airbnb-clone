9c5cd9e7932c92f2a8388a70c82d37f0471bc3e4
NukeStorm <nsdm821@gmail.com>
2020-09-01 23:59:54 +0900

    로그인, 회원가입 폼 생성

---
+++ airbnb/routes/dist/index.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: '테스트 페이지'
+  });
+}); //로그인 요청 POST
+
+router.post('/login', function (req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+}); //회원가입 페이지 요청 GET
+
+router.get('/signup', function (req, res, next) {
+  res.render('signup', {
+    title: '회원가입 페이지'
+  });
+}); //회원가입 요청 POST
+
+router.post('/signup', function (req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+});
+module.exports = router;
--- airbnb/routes/index.js
+++ airbnb/routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '테스트 페이지' });
 });
 
+
+//로그인 요청 POST
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+
+//회원가입 페이지 요청 GET
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: '회원가입 페이지' });
+});
+
+//회원가입 요청 POST
+router.post('/signup', function(req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+});
 module.exports = router;
