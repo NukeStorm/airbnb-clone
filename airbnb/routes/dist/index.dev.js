7f6207f73d4df7cbed4d20a780f858546a7888f6
NukeStorm <nsdm821@gmail.com>
2020-09-02 13:51:08 +0900

    로그인 로그아웃 DB 연동

--- airbnb/routes/dist/index.dev.js
+++ airbnb/routes/dist/index.dev.js
@@ -1,29 +1,90 @@
 "use strict";
 
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
+
+var LevelDB = require("../Util/levelDB");
+
+var userDB = new LevelDB("user");
 /* GET home page. */
 
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: '테스트 페이지'
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "테스트 페이지"
   });
 }); //로그인 요청 POST
 
-router.post('/login', function (req, res, next) {
-  console.log(req.body);
-  res.send(req.body);
+router.post("/login", function (req, res, next) {
+  var id = req.body["id"];
+  var pw = req.body["pw"];
+
+  var loginprocess = function loginprocess() {
+    var user_pw;
+    return regeneratorRuntime.async(function loginprocess$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return regeneratorRuntime.awrap(userDB.get(id));
+
+          case 2:
+            user_pw = _context.sent;
+            return _context.abrupt("return", user_pw);
+
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
+  };
+
+  loginprocess().then(function (user_pw) {
+    console.log('pw:', user_pw);
+
+    if (user_pw === pw) {
+      res.send("\uB85C\uADF8\uC778 \uC131\uACF5 " + id);
+    } else res.send("\uC544\uC774\uB514 \uB610\uB294 \uBE44\uBC00\uBC88\uD638\uAC00 \uD2C0\uB9BC");
+  })["catch"](function (err) {
+    res.send("\uC544\uC774\uB514 \uB610\uB294 \uBE44\uBC00\uBC88\uD638\uAC00 \uD2C0\uB9BC");
+    console.log(err);
+  });
 }); //회원가입 페이지 요청 GET
 
-router.get('/signup', function (req, res, next) {
-  res.render('signup', {
-    title: '회원가입 페이지'
+router.get("/signup", function (req, res, next) {
+  res.render("signup", {
+    title: "회원가입 페이지"
   });
 }); //회원가입 요청 POST
 
-router.post('/signup', function (req, res, next) {
+router.post("/signup", function (req, res, next) {
   console.log(req.body);
-  res.send(req.body);
+  var id = req.body["id"];
+  var pw = req.body["pw"];
+
+  var signupprocess = function signupprocess() {
+    return regeneratorRuntime.async(function signupprocess$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            console.log(id);
+            _context2.next = 3;
+            return regeneratorRuntime.awrap(userDB.get(id).then(function (val) {
+              res.send("이미 등록된 회원" + val);
+            })["catch"](function (err) {
+              userDB.put(id, pw).then(res.send("\uD68C\uC6D0\uAC00\uC785 \uC644\uB8CC: ".concat(id)));
+              console.log(err);
+            }));
+
+          case 3:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    });
+  };
+
+  signupprocess();
 });
 module.exports = router;
--- airbnb/routes/index.js
+++ airbnb/routes/index.js
@@ -1,27 +1,60 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const LevelDB = require("../Util/levelDB");
+const userDB = new LevelDB("user");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '테스트 페이지' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "테스트 페이지" });
 });
-
 
 //로그인 요청 POST
-router.post('/login', function(req, res, next) {
-  console.log(req.body);
-  res.send(req.body);
+router.post("/login", function (req, res, next) {
+  let id = req.body["id"];
+  let pw = req.body["pw"];
+
+  let loginprocess = async () => {
+    let user_pw = await userDB.get(id);
+    return user_pw;
+  };
+
+  loginprocess().then((user_pw)=>{
+    console.log('pw:', user_pw);
+    if (user_pw === pw) {
+      res.send(`로그인 성공 ` + id);
+    } else res.send(`아이디 또는 비밀번호가 틀림`);
+
+  }).catch((err) => {
+    res.send(`아이디 또는 비밀번호가 틀림`);
+    console.log(err);
+  });
+
+
+ 
 });
 
-
 //회원가입 페이지 요청 GET
-router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: '회원가입 페이지' });
+router.get("/signup", function (req, res, next) {
+  res.render("signup", { title: "회원가입 페이지" });
 });
 
 //회원가입 요청 POST
-router.post('/signup', function(req, res, next) {
+router.post("/signup", function (req, res, next) {
   console.log(req.body);
-  res.send(req.body);
+  let id = req.body["id"];
+  let pw = req.body["pw"];
+  let signupprocess = async () => {
+    console.log(id);
+    await userDB
+      .get(id)
+      .then((val) => {
+        res.send("이미 등록된 회원" + val);
+      })
+      .catch((err) => {
+        userDB.put(id, pw).then(res.send(`회원가입 완료: ${id}`));
+        console.log(err);
+      });
+  };
+  signupprocess();
 });
+
 module.exports = router;
