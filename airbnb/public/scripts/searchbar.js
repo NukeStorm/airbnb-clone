767e30ab7644b96c1ea8e3f839586f85bca3dfa1
NukeStorm <nsdm821@gmail.com>
2020-09-09 22:17:30 +0900

    달력 초기값 변경

--- airbnb/public/scripts/searchbar.js
+++ airbnb/public/scripts/searchbar.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 function initCalander() {
   let todayStr = new Date().toISOString().split("T")[0];
-  document.getElementsByName("trip-start")[0].setAttribute("value", todayStr);
+  document.getElementsByName("trip-start")[0].setAttribute("value", null);
   document.getElementsByName("trip-start")[0].setAttribute("min", todayStr);
-  document.getElementsByName("trip-end")[0].setAttribute("value", todayStr);
+  document.getElementsByName("trip-end")[0].setAttribute("value", null);
   document.getElementsByName("trip-end")[0].setAttribute("min", todayStr);
 }
 initCalander();
