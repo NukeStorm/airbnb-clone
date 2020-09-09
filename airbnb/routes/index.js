ed90aeda388cf188df51336ebc77cab816238561
NukeStorm <nsdm821@gmail.com>
2020-09-09 10:52:51 +0900

    js 코딩컨벤션에 맞게 수정, 코드 최적화 eslint(airbnb-base), prettier 사용, 코드구조 개선

--- airbnb/routes/index.js
+++ airbnb/routes/index.js
@@ -1,16 +1,14 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
 const LevelDB = require("../Util/levelDB");
-const userDB = new LevelDB("user");
 const RoomDAO = require("../model/RoomDao");
-const Room = require("../model/Room");
+
+const router = express.Router();
+const userDB = new LevelDB("user");
 const roomDao = new RoomDAO();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  let manager = res.locals.manager;
-  console.dir(manager, { depth: null });
-  console.log(res.locals);
+router.get("/", (req, res, next) => {
+  let { manager } = res.locals;
 
   let userid = res.locals.session;
 
@@ -19,100 +17,89 @@ router.get("/", function (req, res, next) {
   });
 });
 
-//로그인 요청 POST
-router.post("/login", function (req, res, next) {
-  let id = req.body["id"];
-  let pw = req.body["pw"];
+// 로그인 요청 POST
+router.post("/login", (req, res, next) => {
+  let { id } = req.body;
+  let { pw } = req.body;
 
   let loginprocess = async () => {
-    let user_pw = await userDB.get(id);
-    return user_pw;
+    let userPw = await userDB.get(id);
+    return userPw;
   };
 
-  let manager = res.locals.manager;
+  let { manager } = res.locals;
 
   loginprocess()
-    .then((user_pw) => {
-      console.log("pw:", user_pw);
-      if (user_pw === pw) {
+    .then((userPw) => {
+      if (userPw === pw) {
         let newSessionId = manager.setSession(id);
-        if (!req.cookies["EXPRESS_SESSION"]) {
-          //expires: new Date(Date.now() + 900000)
-          //console.dir(manager.setSession(id), {depth:null});
+        if (!req.cookies.EXPRESS_SESSION) {
+          // expires: new Date(Date.now() + 900000)
+          // console.dir(manager.setSession(id), {depth:null});
           res.cookie("EXPRESS_SESSION", newSessionId, { httpOnly: true });
         } else {
-          manager.removeSession(req.cookies["EXPRESS_SESSION"]);
+          manager.removeSession(req.cookies.EXPRESS_SESSION);
           res.cookie("EXPRESS_SESSION", newSessionId, { httpOnly: true });
         }
         // res.send(`로그인 성공 ` + id);
         res.redirect("../");
         res.render("index", { title: `로그인ID : ${id} 테스트 페이지` });
-      } else res.send(`아이디 또는 비밀번호가 틀림`);
+      } else res.render("index", { msg: `아이디 또는 비밀번호가 틀림` });
     })
     .catch((err) => {
-      res.send(`아이디 또는 비밀번호가 틀림`);
-      console.log(err);
+      res.render("index", { msg: `아이디 또는 비밀번호가 틀림` });
     });
 });
 
-//로그아웃 요청 GET
+// 로그아웃 요청 GET
 
-router.get("/logout", function (req, res, next) {
-  let manager = res.locals.manager;
-  let sessionId = req.cookies["EXPRESS_SESSION"];
+router.get("/logout", (req, res, next) => {
+  let { manager } = res.locals;
+  let sessionId = req.cookies.EXPRESS_SESSION;
   if (sessionId) {
     manager.removeSession(sessionId);
     res.locals.session = null;
     res.clearCookie("EXPRESS_SESSION");
   }
   res.redirect("../");
-  //res.render(  "index", { user:false,  test:'test',title: `테스트 페이지`});
+  // res.render(  "index", { user:false,  test:'test',title: `테스트 페이지`});
 });
 
-//회원가입 페이지 요청 GET
-router.get("/signup", function (req, res, next) {
+// 회원가입 페이지 요청 GET
+router.get("/signup", (req, res, next) => {
   res.render("signup", { title: "회원가입 페이지" });
 });
 
-//회원가입 요청 POST
-router.post("/signup", function (req, res, next) {
-  console.log(req.body);
-  let id = req.body["id"];
-  let pw = req.body["pw"];
+// 회원가입 요청 POST
+router.post("/signup", (req, res, next) => {
+  let { id } = req.body;
+  let { pw } = req.body;
   let signupprocess = async () => {
-    console.log(id);
     await userDB
       .get(id)
       .then((val) => {
-        res.send("이미 등록된 회원" + val);
+        res.render("index", { msg: `이미 등록된 이메일ID입니다` });
       })
       .catch((err) => {
-        userDB.put(id, pw).then(res.send(`회원가입 완료: ${id}`));
-        console.log(err);
+        userDB.put(id, pw).then(res.render("index", { msg: `회원가입이 완료되었습니다.` }));
       });
   };
   signupprocess();
 });
 
-//숙소정보 검색 요청 POST
-router.post("/search", function (req, res, next) {
-  console.log(req.body);
+// 숙소정보 검색 요청 POST
+router.post("/search", (req, res, next) => {
+  let targetPos = req.body.pos;
+  let targetNum = parseInt(req.body.personNum, 10);
 
-  let target_pos = req.body["pos"];
-  let target_num = parseInt(req.body["personNum"]);
-  
-  if(!target_pos)
-  target_pos="";
+  if (!targetPos) targetPos = "";
 
-  if(!target_num)
-  target_num=0;
+  if (!targetNum) targetNum = 0;
 
-  console.log(target_pos, target_num);
-  let roomlist = roomDao.findRoomby(function (room) {
-    return room.pos.search(target_pos) >= 0 && room.maxnum >= target_num;
+  let roomlist = roomDao.findRoomby((room) => {
+    return room.pos.search(targetPos) >= 0 && room.maxnum >= targetNum;
   });
-  res.render("searchresult", { result: roomlist ,target_pos:target_pos, target_num:target_num  });
-  
+  res.render("searchresult", { result: roomlist, target_pos: targetPos, target_num: targetNum });
 });
 
 module.exports = router;
